69d72e221a9b4cffb9307d3d63b12aec6e07accc
unknown <marcel_de_bree@hotmail.com>
2019-06-12 17:57:18 +0200

    error msg

--- scapy/modules/winpcapy.py
+++ scapy/modules/winpcapy.py
@@ -317,8 +317,8 @@ try:  # NPCAP ONLY function
     pcap_activate.argtypes = [POINTER(pcap_t)]
 
     pcap_statustostr = _lib.pcap_statustostr
-    pcap_statustostr = c_wchar_p
-    pcap_statustostr = [POINTER(c_int)]
+    pcap_statustostr.restype = c_wchar_p
+    pcap_statustostr.argtypes = [POINTER(c_int)]
 except AttributeError:
     pass
 
