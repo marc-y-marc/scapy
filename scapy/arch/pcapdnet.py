783b78343fa121facaa47b29ed442ca030637792
unknown <marcel_de_bree@hotmail.com>
2019-06-13 08:47:02 +0200

    revert all

--- scapy/arch/pcapdnet.py
+++ scapy/arch/pcapdnet.py
@@ -199,7 +199,7 @@ if conf.use_pcap:
                     pcap_set_timeout, pcap_set_rfmon, pcap_activate
                 self.pcap = pcap_create(self.iface, self.errbuf)
                 pcap_set_snaplen(self.pcap, snaplen)
-                #pcap_set_promisc(self.pcap, promisc)
+                pcap_set_promisc(self.pcap, promisc)
                 pcap_set_timeout(self.pcap, to_ms)
                 if pcap_set_rfmon(self.pcap, 1) != 0:
                     warning("Could not set monitor mode")
